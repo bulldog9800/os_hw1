7abae4d01c67e1e56946d1abdbdf294ecdb7400a
fesal <fisal-ab@campus.technion.ac.il>
2021-05-01 13:18:29 +0000

    implement ctrl-c handler

--- signals.cpp
+++ signals.cpp
@@ -6,13 +6,22 @@
 using namespace std;
 
 void ctrlZHandler(int sig_num) {
-	// TODO: Add your implementation
+    std::cout << "smash: got ctrl-Z" << std::endl;
+
 }
 
 void ctrlCHandler(int sig_num) {
     std::cout << "smash: got ctrl-C" << std::endl;
-    if ()
-
+    SmallShell& smash= SmallShell::getInstance() ;
+    if (smash.pid_in_fg==0){
+        return;
+    }
+    if (kill(smash.pid_in_fg, SIGKILL)==-1){
+        perror("smash error: kill failed");
+        return;
+    }
+    cout << "smash: process " << smash.pid_in_fg << " was killed" << endl;
+    smash.pid_in_fg = 0;
 }
 
 void alarmHandler(int sig_num) {
